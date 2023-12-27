604950a0c89a87e4c0e53ef6390c3d6ac1c9e8e1
samar abdelhmeed <samar.abdelhmeed77@gmail.com>
2023-12-27 14:23:53 +0200

    add home page and tailwandcss style

--- packages/chainlist2/astro.config.mjs
+++ packages/chainlist2/astro.config.mjs
@@ -1,7 +1,12 @@
-import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
-// https://astro.build/config
-export default defineConfig({
-  integrations: [react()]
-});
+export default {
+  integrations: [react()],
+  styles: [
+    '@app/styles.css', // Add this line to include your custom styles
+  ],
+  routes: {
+    '/explore': '/src/pages/index.astro', // Adjust the path based on your project structure
+  },
+  // ... rest of your config
+};
---
+++ packages/chainlist2/postcss.config.cjs
@@ -0,0 +1,8 @@
+// postcss.config.cjs
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    // Other PostCSS plugins as needed...
+  },
+};
